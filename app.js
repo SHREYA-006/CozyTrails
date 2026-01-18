ebcd12db9fdebc0c2bbbdaa632dfe79095854e28
Shreya Yadav <shreyayadav9885@gmail.com>
2026-01-18 18:17:27 +0530

    managing currentuser

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.failure=req.flash("failure");
     res.locals.error = req.flash("error");
-    res.locals.currentUser=req.user;
+    res.locals.currentUser=req.user||null;
     next();
 })
 
